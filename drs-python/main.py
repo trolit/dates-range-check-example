e111c240e442b37f4d5d4098ec5b845df5d30d33
Paweł Idzikowski <iddziku@gmail.com>
2020-09-19 13:16:57 +0200

    Update main.py

--- drs-python/main.py
+++ drs-python/main.py
@@ -1,11 +1,11 @@
 # This is a sample in Python for
-# Dates Range Check(DRS)
+# subject: Dates Range Check(DRS)
 # created by: https://github.com/trolit
 
 # Example
 # Below code will validate if it's possible to rent house between given dates
 # with "fake" collection. Real version with db connection can be extended to
-# return for e.g. rents that dates are later than current one.
+# return for e.g. rents that dates are later than current one and only lasting ones.
 
 import datetime
 
@@ -15,15 +15,17 @@ class Rent:
         self.start_date = start_date
         self.end_date = end_date
 
+
 # https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-python
 class Color:
     OKBLUE = '\033[94m'
     FAIL = '\033[91m'
     ENDC = '\033[0m'
 
-# 27.6.2020 - 15.7.2020
-# 23.7.2020 - 26.7.2020
-# 4.8.2020 - 19.8.2020
+
+# R1: 27.6.2020 - 15.7.2020
+# R2: 23.7.2020 - 26.7.2020
+# R3: 4.8.2020 - 19.8.2020
 def get_booked_dates():
     var_rent1 = Rent(datetime.date(2020, 6, 27), datetime.date(2020, 7, 15))
     var_rent2 = Rent(datetime.date(2020, 7, 23), datetime.date(2020, 7, 26))
@@ -42,7 +44,7 @@ def validate_rent(rent):
         if rent.start_date < booked_date.end_date and rent.end_date > booked_date.start_date:
             return f'Rent between {adjust_date_format(rent.start_date)} and {adjust_date_format(rent.end_date)}' \
                    f' is not available' f' due to {adjust_date_format(booked_date.start_date)} - ' \
-                   f'{adjust_date_format(booked_date.end_date)} reservation.'
+                   f'{adjust_date_format(booked_date.end_date)} reservation ({Color.FAIL}R{(var_index + 1)}{Color.ENDC})'
         var_index += 1
     return f'Rent between {adjust_date_format(rent.start_date)} and {adjust_date_format(rent.end_date)} is available.'
 
